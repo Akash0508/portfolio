8749692ba1d929fe371a82aab083b698fcb6270c
Priyansh Singh <singhpriyansh2000@gmail.com>
2024-10-13 22:36:03 +0530

    fix navbar for mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   FaInstagram,
   FaTwitter,
   FaGithub,
   FaMoon,
   FaSun,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
 
 const Navbar: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("darkMode") === "true";
@@ -24,34 +28,40 @@ const Navbar: React.FC = () => {
     document.documentElement.classList.toggle("dark", newDarkMode);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const NavLink = ({ to, children }: { to: string; children: React.ReactNode }) => (
+    <Link
+      to={to}
+      className={`block py-2 px-4 text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300 ${
+        location.pathname === to ? "font-bold" : ""
+      }`}
+      onClick={() => setIsMenuOpen(false)}
+    >
+      {children}
+    </Link>
+  );
+
   return (
     <nav className="w-full bg-white dark:bg-black border-y border-gray-200 dark:border-gray-800">
-      <div className="w-[53%] mx-auto">
+      <div className="w-full md:w-[53%] mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <Link
-            to="/"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300"
-          >
-            Home
-          </Link>
-          <Link
-            to="/about"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300"
-          >
-            About
-          </Link>
-          <Link
-            to="/blog"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300"
-          >
-            Blogs
-          </Link>
-          <Link
-            to="/projects"
-            className="text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-300"
-          >
-            Projects
-          </Link>
+          <div className="flex items-center">
+            <button
+              className="md:hidden text-gray-800 dark:text-white"
+              onClick={toggleMenu}
+            >
+              {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+            <div className="hidden md:flex space-x-4">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/blog">Blogs</NavLink>
+              <NavLink to="/projects">Projects</NavLink>
+            </div>
+          </div>
           <div className="flex items-center space-x-4">
             <a
               href="https://github.com/leviackerman05"
@@ -91,7 +101,7 @@ const Navbar: React.FC = () => {
                 />
                 <div className="w-10 h-4 bg-gray-600 dark:bg-gray-600 rounded-full shadow-inner"></div>
                 <div
-                  className={`absolute w-6 h-6  rounded-full shadow transition-transform duration-300 ease-in-out flex items-center -left-1 -top-1 justify-center ${
+                  className={`absolute w-6 h-6 rounded-full shadow transition-transform duration-300 ease-in-out flex items-center -left-1 -top-1 justify-center ${
                     darkMode
                       ? "translate-x-full bg-gray-300"
                       : "translate-x-0 bg-black"
@@ -110,6 +120,14 @@ const Navbar: React.FC = () => {
             </label>
           </div>
         </div>
+        {isMenuOpen && (
+          <div className="md:hidden">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/blog">Blogs</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+          </div>
+        )}
       </div>
     </nav>
   );
